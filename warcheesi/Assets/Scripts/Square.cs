2437d97207eb71227b37eed4319446b1dd365fb7
pjsillescas <pjsillescas@gmail.com>
2023-08-11 18:22:31 +0200

    Tablero básico con los caminos de las fichas.

---
+++ warcheesi/Assets/Scripts/Square.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer Mesh;
+
+    [SerializeField]
+    private List<Transform> TokenPositions;
+
+    [SerializeField]
+    private List<Square> NextSquares;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
